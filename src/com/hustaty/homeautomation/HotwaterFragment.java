5d8ae9493a3a868c37fd8c6b514a5467426e44fd
slavomir.hustaty@gmail.com <slavomir.hustaty@gmail.com>
2013-02-24 23:44:37 +0100

    added functionality to add stored event

--- src/com/hustaty/homeautomation/HotwaterFragment.java
+++ src/com/hustaty/homeautomation/HotwaterFragment.java
@@ -2,14 +2,18 @@ package com.hustaty.homeautomation;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.TimePicker;
-import android.widget.ToggleButton;
+import android.widget.*;
+import com.hustaty.homeautomation.enums.Appliance;
+import com.hustaty.homeautomation.enums.Command;
+import com.hustaty.homeautomation.exception.HomeAutomationException;
+import com.hustaty.homeautomation.http.MyHttpClient;
+import com.hustaty.homeautomation.model.CommonResult;
 
+import java.io.IOException;
 import java.util.Calendar;
 
 /**
@@ -18,17 +22,23 @@ import java.util.Calendar;
  * Time: 7:20 PM
  */
 public class HotwaterFragment extends Fragment {
+
+    //Logging support.
+    private static final String LOG_TAG = HotwaterFragment.class.getName();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.hotwater_fragment, container, false);
 
-        DatePicker datePicker = (DatePicker)view.findViewById(R.id.hotwater_date_from);
-        TimePicker timePicker = (TimePicker)view.findViewById(R.id.hotwater_time_from);
+        final DatePicker datePicker = (DatePicker)view.findViewById(R.id.hotwater_date_from);
+        final TimePicker timePicker = (TimePicker)view.findViewById(R.id.hotwater_time_from);
 
-        ToggleButton wishedState = (ToggleButton)view.findViewById(R.id.hotwater_wished_state);
+        final ToggleButton wishedState = (ToggleButton)view.findViewById(R.id.hotwater_wished_state);
 
-        EditText hotwaterValidity = (EditText)view.findViewById(R.id.hotwater_validity);
+        final EditText hotwaterValidity = (EditText)view.findViewById(R.id.hotwater_validity);
+
+        final ImageButton hotWaterSave = (ImageButton)view.findViewById(R.id.hotwater_save);
 
         //default to next day at 07:15 in the morning
         Calendar calendar = Calendar.getInstance();
@@ -45,6 +55,32 @@ public class HotwaterFragment extends Fragment {
         hotwaterValidity.setText("60");
 
         wishedState.setChecked(true);
+
+        hotWaterSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Calendar cal = Calendar.getInstance();
+                cal.set(Calendar.YEAR, datePicker.getYear());
+                cal.set(Calendar.MONTH, datePicker.getMonth());
+                cal.set(Calendar.DAY_OF_MONTH, datePicker.getDayOfMonth());
+                cal.set(Calendar.HOUR_OF_DAY, timePicker.getCurrentHour());
+                cal.set(Calendar.MINUTE, timePicker.getCurrentMinute());
+
+                MyHttpClient myHttpClient = new MyHttpClient(view.getContext());
+                Calendar cal2 = Calendar.getInstance();
+                cal2.setTime(cal.getTime());
+                cal2.add(Calendar.MINUTE, new Integer(hotwaterValidity.getText().toString()));
+                try {
+                    CommonResult commonResult = myHttpClient.addStoredEvent(Appliance.HOTWATER, Command.ON, cal.getTime(), cal2.getTime());
+                } catch (IOException e) {
+                    Log.e(LOG_TAG, e.getMessage());
+                } catch (HomeAutomationException e) {
+                    Log.e(LOG_TAG, e.getMessage());
+                }
+
+            }
+        });
 
         return view;
     }
