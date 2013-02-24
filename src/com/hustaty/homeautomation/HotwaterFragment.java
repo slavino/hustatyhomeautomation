54b61141473941b30906c4fae4a1e05052c8cdd1
slavomir.hustaty@gmail.com <slavomir.hustaty@gmail.com>
2013-02-24 23:20:09 +0100

    added layout for date/time pickers

--- src/com/hustaty/homeautomation/HotwaterFragment.java
+++ src/com/hustaty/homeautomation/HotwaterFragment.java
@@ -8,6 +8,9 @@ import android.view.ViewGroup;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TimePicker;
+import android.widget.ToggleButton;
+
+import java.util.Calendar;
 
 /**
  * User: llisivko
@@ -22,8 +25,26 @@ public class HotwaterFragment extends Fragment {
 
         DatePicker datePicker = (DatePicker)view.findViewById(R.id.hotwater_date_from);
         TimePicker timePicker = (TimePicker)view.findViewById(R.id.hotwater_time_from);
-//        NumberPicker numberPicker = (NumberPicker)view.findViewById(R.id.hotwater_validity);
 
+        ToggleButton wishedState = (ToggleButton)view.findViewById(R.id.hotwater_wished_state);
+
+        EditText hotwaterValidity = (EditText)view.findViewById(R.id.hotwater_validity);
+
+        //default to next day at 07:15 in the morning
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH ,1);
+        calendar.set(Calendar.HOUR_OF_DAY, 7);
+        calendar.set(Calendar.MINUTE, 15);
+
+        datePicker.updateDate(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+
+        timePicker.setIs24HourView(true);
+        timePicker.setCurrentHour(calendar.get(Calendar.HOUR_OF_DAY));
+        timePicker.setCurrentMinute(calendar.get(Calendar.MINUTE));
+
+        hotwaterValidity.setText("60");
+
+        wishedState.setChecked(true);
 
         return view;
     }
