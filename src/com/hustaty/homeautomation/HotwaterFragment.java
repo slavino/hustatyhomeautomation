d42134234a39044897b5d1874ea2d6a5780efefa
slavomir.hustaty@gmail.com <slavomir.hustaty@gmail.com>
2013-02-24 22:38:59 +0100

    added layout for date/time pickers

--- src/com/hustaty/homeautomation/HotwaterFragment.java
+++ src/com/hustaty/homeautomation/HotwaterFragment.java
@@ -5,21 +5,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
 /**
- * Created with IntelliJ IDEA.
  * User: llisivko
  * Date: 2/24/13
  * Time: 7:20 PM
- * To change this template use File | Settings | File Templates.
  */
 public class HotwaterFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
-
         View view = inflater.inflate(R.layout.hotwater_fragment, container, false);
+
+        DatePicker datePicker = (DatePicker)view.findViewById(R.id.hotwater_date_from);
+        TimePicker timePicker = (TimePicker)view.findViewById(R.id.hotwater_time_from);
+//        NumberPicker numberPicker = (NumberPicker)view.findViewById(R.id.hotwater_validity);
+
 
         return view;
     }
