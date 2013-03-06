5b84fda76aa4a0a5b890f573d7156200aa199840
slavomir.hustaty@gmail.com <slavomir.hustaty@gmail.com>
2013-03-06 21:14:48 +0100

    ui changes for notifications

--- src/com/hustaty/homeautomation/HeatingFragment.java
+++ src/com/hustaty/homeautomation/HeatingFragment.java
@@ -6,13 +6,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import javax.crypto.*;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
+//import javax.crypto.*;
+//import java.io.File;
+//import java.io.FileOutputStream;
+//import java.io.IOException;
+//import java.io.ObjectOutputStream;
+//import java.security.InvalidKeyException;
+//import java.security.NoSuchAlgorithmException;
 
 /**
  * User: llisivko
@@ -25,56 +25,56 @@ public class HeatingFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.heating_fragment, container, false);
 
-        SecretKey key = null;
-        try {
-            key = KeyGenerator.getInstance("DES").generateKey();
-            writeToFile("secretkey.dat", key);
-            Cipher cipher = Cipher.getInstance("DES");
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-            javax.crypto.SealedObject sealedObject = new SealedObject("message", cipher);
-            writeToFile("sealed.dat", sealedObject);
-
-
-            String algorithmName = sealedObject.getAlgorithm();
-            Cipher cipher2 = Cipher.getInstance(algorithmName);
-            cipher.init(Cipher.DECRYPT_MODE, key);
-            String text = (String) sealedObject.getObject(cipher);
-
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        } catch (NoSuchPaddingException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        } catch (IllegalBlockSizeException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        } catch (InvalidKeyException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        } catch (Exception e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
+//        SecretKey key = null;
+//        try {
+//            key = KeyGenerator.getInstance("AES").generateKey();
+//            writeToFile("secretkey.dat", key);
+//            Cipher cipher = Cipher.getInstance("AES");
+//            cipher.init(Cipher.ENCRYPT_MODE, key);
+//            javax.crypto.SealedObject sealedObject = new SealedObject("message", cipher);
+//            writeToFile("sealed.dat", sealedObject);
+//
+//
+//            String algorithmName = sealedObject.getAlgorithm();
+//            Cipher cipher2 = Cipher.getInstance(algorithmName);
+//            cipher.init(Cipher.DECRYPT_MODE, key);
+//            String text = (String) sealedObject.getObject(cipher);
+//
+//        } catch (NoSuchAlgorithmException e) {
+//            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+//        } catch (NoSuchPaddingException e) {
+//            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+//        } catch (IllegalBlockSizeException e) {
+//            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+//        } catch (InvalidKeyException e) {
+//            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+//        } catch (IOException e) {
+//            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+//        } catch (Exception e) {
+//            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+//        }
 
         return view;
     }
 
-    private static void writeToFile(String filename, Object object) throws Exception {
-        FileOutputStream fos = null;
-        ObjectOutputStream oos = null;
-
-        try {
-            fos = new FileOutputStream(new File(filename));
-            oos = new ObjectOutputStream(fos);
-            oos.writeObject(object);
-            oos.flush();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (oos != null) {
-                oos.close();
-            }
-            if (fos != null) {
-                fos.close();
-            }
-        }
-    }
+//    private static void writeToFile(String filename, Object object) throws Exception {
+//        FileOutputStream fos = null;
+//        ObjectOutputStream oos = null;
+//
+//        try {
+//            fos = new FileOutputStream(new File(filename));
+//            oos = new ObjectOutputStream(fos);
+//            oos.writeObject(object);
+//            oos.flush();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        } finally {
+//            if (oos != null) {
+//                oos.close();
+//            }
+//            if (fos != null) {
+//                fos.close();
+//            }
+//        }
+//    }
 }
--- src/com/hustaty/homeautomation/service/TrafficNotificationService.java
+++ src/com/hustaty/homeautomation/service/TrafficNotificationService.java
@@ -5,6 +5,9 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import com.hustaty.homeautomation.R;
 import com.hustaty.homeautomation.util.LogUtil;
@@ -22,9 +25,17 @@ public class TrafficNotificationService {
     public TrafficNotificationService(Context context, String notificationText, boolean silentNotification) {
 
         NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
-        Notification notification = new Notification(R.drawable.home, "Traffic: Police on way home!", System.currentTimeMillis());
+//        Notification notification = new Notification(R.drawable.home, "Traffic: Police on way home!", System.currentTimeMillis());
 
         String notificationTitle = "Traffic Information";
+
+        Bitmap largeIcon = BitmapFactory.decodeResource(context.getResources(), R.drawable.home);
+        Notification notification = new NotificationCompat.Builder(context).setContentTitle("Traffic: Police on way home!")
+                .setContentText(notificationTitle)
+                .setSmallIcon(R.drawable.home_notification)
+                .setLargeIcon(largeIcon)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(notificationText))
+                .build();
 
 //        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(notificationText));
 //        Launch traffic information provider's application
